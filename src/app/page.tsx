8cf61ddf20ca908acb3af6f28400eea3db1b6ca9
space1worm <kverenchkhiladzeirakli.work@gmail.com>
2024-06-28 11:04:09 +0400

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex size-full h-screen items-center justify-center">
+      <h1 className="text-3xl">Lets go.</h1>
+    </main>
+  );
+}
