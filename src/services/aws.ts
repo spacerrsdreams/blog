6e45c3921d6278a3e47fc661b9ebee1b42088bc6
spacerr <kverenchkhiladzeirakli.personal@gmail.com>
2024-08-11 14:27:21 +0400

    refactored services, started using axios

---
+++ src/services/aws.ts
@@ -0,0 +1,50 @@
+import { useMutation } from "@tanstack/react-query";
+
+import { ROUTES } from "@/utils/routes";
+
+type UploadPayload = {
+  filename: string;
+  contentType: string;
+  file: File;
+};
+
+export const useUploadImage = () => {
+  return useMutation({
+    mutationFn: async ({ file, contentType, filename }: UploadPayload) => {
+      const res = await fetch(ROUTES.api.image.upload, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ contentType, filename }),
+      });
+
+      if (res.ok) {
+        const { url, fields } = await res.json();
+
+        const formData = new FormData();
+        Object.entries(fields).forEach(([key, value]) => {
+          formData.append(key, value as string);
+        });
+
+        formData.append("file", file);
+
+        const uploadResponse = await fetch(url, {
+          method: "POST",
+          body: formData,
+        });
+
+        const imageUrl = `${url}${fields.key}`;
+
+        if (!uploadResponse.ok) {
+          throw new Error("Upload failed.");
+        }
+
+        // Step 4: Check if the response has content before parsing as JSON
+        return imageUrl;
+      } else {
+        throw new Error("Failed to get pre-signed URL.");
+      }
+    },
+  });
+};
