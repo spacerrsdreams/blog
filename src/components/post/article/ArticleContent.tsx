8d5701bd34381b21c90b5291b7fd07b35b009142
lukannozadze <nozadzeluka2002@gmail.com>
2024-08-20 19:25:07 +0400

    refactored article page for mobile

--- src/components/post/article/ArticleContent.tsx
+++ src/components/post/article/ArticleContent.tsx
@@ -9,7 +9,7 @@ export default function ArticleContent({ postContent }: { postContent: Value }) 
   return (
     <ReactQuill
       theme="bubble"
-      className="max-w-[680px] text-lg text-muted-foreground"
+      className="max-w-[680px] px-0 text-lg text-muted-foreground"
       value={postContent}
       readOnly={true}
     />
