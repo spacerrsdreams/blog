6248cb0d8e33283596c75372277c60d26a79f769
lukannozadze <nozadzeluka2002@gmail.com>
2024-09-22 12:20:37 +0400

    added restriction for non-admin users

--- src/app/create-article/page.tsx
+++ src/app/create-article/page.tsx
@@ -1,5 +1,21 @@
+import { RedirectToSignIn } from "@clerk/nextjs";
+import { auth, clerkClient } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+import { ROUTES } from "@/utils/routes";
 import CreateArticle from "@/components/post/article/CreateArticle";
 
-export default function page() {
-  return <CreateArticle />;
+export default async function page() {
+  const { userId } = auth();
+
+  if (!userId) {
+    return <RedirectToSignIn />;
+  }
+
+  const user = await clerkClient().users.getUser(userId);
+
+  if (user.privateMetadata?.role === "admin") {
+    return <CreateArticle />;
+  }
+  redirect(ROUTES.root);
 }
