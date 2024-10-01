349a74e53da4e52c01a9a27a936e78b27cb3ecf4
lukannozadze <nozadzeluka2002@gmail.com>
2024-10-01 18:22:09 +0400

    refactored code

--- src/services/post/article.ts
+++ src/services/post/article.ts
@@ -1,3 +1,5 @@
+import { Prisma } from "@prisma/client";
+
 import { useMutation } from "@tanstack/react-query";
 
 import request from "@/utils/request";
@@ -60,3 +62,10 @@ export const useEditArticle = () => {
     },
   });
 };
+export const topPostsByAuthorsQuery: Prisma.Sql = Prisma.sql`
+SELECT DISTINCT ON (p."authorId") p.*, u.first_name, u.last_name, u.image_url
+FROM posts p
+INNER JOIN users u ON u.id = p."authorId"
+ORDER BY p."authorId", p."likeCount" DESC
+LIMIT 3;
+`;
